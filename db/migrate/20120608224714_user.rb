4e37a0e0677e43bd2859d60c07f45645e9da8d05
Juan David Pastas <juan.david@codescrum.com>
2012-06-12 11:16:58 -0500

    added some unversioned files

---
+++ db/migrate/20120608224714_user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :name
+      t.string :type
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
