1495a578864b77acc9a87d2c75019a1dbd6ebb10
Juan David Pastas <juan.david@codescrum.com>
2012-06-15 15:00:08 -0500

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 3.times do
-  tenant = Tenant.create name: 'tenant' + (Tenant.count + 1)
-  landlord = Landlord.create name: 'landlord' + (Landlord.count + 1)
+  tenant = Tenant.create name: 'tenant' + (Tenant.count + 1).to_s
+  landlord = Landlord.create name: 'landlord' + (Landlord.count + 1).to_s
   Meeting.create landlord: landlord, tenant: tenant, at: Time.now
 end
